d3626d69688b7410369c2408ce4889fd0f84c1b3
Taing korngmeng <korngmeng015@gmail.com>
2026-06-02 18:13:53 +0700

    Remove debug scripts

--- run_producer.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-from api.crypto_producer import produce
-produce()
